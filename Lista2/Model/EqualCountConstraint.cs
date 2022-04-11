a581287486c5b6ea5f55fdeeb455f60fe9bebbe0
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-04-11 20:35:15 +0200

    variables + domains

---
+++ Lista2/Model/EqualCountConstraint.cs
@@ -0,0 +1,24 @@
+﻿namespace Lista2.Model
+{
+    public class EqualCountConstraint : Constraint<Field, int>
+    {
+        public EqualCountConstraint(List<Field> variables) : base(variables)
+        {
+
+        }
+
+        public override bool IsStisfied(Dictionary<Field, int> assignement)
+        {
+            int count = 0;
+            foreach (Field field in Variables)
+            {
+                if (!assignement.ContainsKey(field))
+                {
+                    return true;
+                }
+                count += assignement[field];
+            }
+            return count * 2 == Variables.Count;
+        }
+    }
+}
--- Lista2/Program.cs
+++ Lista2/Program.cs
@@ -1,35 +1,82 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Lista2.Model;
 
-var westernAustralia = "Western Australia";
-var northernTerritory = "Northern Territory";
-var southAustralia = "South Australia";
-var queensland = "Queensland";
-var newSouthWales = "New South Wales";
-var victoria = "Victoria";
-var tasmania = "Tasmania";
+var variables = new List<Field>();
+for (int i = 0; i < 6; i++)
+{
+    for (int j = 0; j < 6; j++)
+    {
+        variables.Add(new Field() { Row = i, Column = j });
+    }
+}
 
-var variables = new List<string> { westernAustralia, northernTerritory, southAustralia, 
-    queensland, newSouthWales, victoria, tasmania};
-
-var domains = new Dictionary<string, List<string>>();
+var domains = new Dictionary<Field, List<int>>();
 foreach (var variable in variables)
 {
-    domains.Add(variable, new List<string> { "red", "green", "blue"});
+    domains.Add(variable, new List<int> { 0, 1});
 }
-var csp = new CSP<string, string>(variables, domains);
-csp.AddConstraint(new MapColoringConstraint(westernAustralia, northernTerritory));
-csp.AddConstraint(new MapColoringConstraint(westernAustralia, southAustralia));
-csp.AddConstraint(new MapColoringConstraint(southAustralia, northernTerritory));
-csp.AddConstraint(new MapColoringConstraint(queensland, northernTerritory));
-csp.AddConstraint(new MapColoringConstraint(queensland, southAustralia));
-csp.AddConstraint(new MapColoringConstraint(queensland, newSouthWales));
-csp.AddConstraint(new MapColoringConstraint(newSouthWales, southAustralia));
-csp.AddConstraint(new MapColoringConstraint(victoria, southAustralia));
-csp.AddConstraint(new MapColoringConstraint(victoria, newSouthWales));
-csp.AddConstraint(new MapColoringConstraint(victoria, tasmania));
 
-var solution = csp.Backtracking(new Dictionary<string, string>());
+var constant_0_0 = variables.First(v => v.Row == 0 && v.Column == 0);
+domains[constant_0_0] = new List<int> { 1 };
+
+var constant_0_3 = variables.First(v => v.Row == 0 && v.Column == 3);
+domains[constant_0_3] = new List<int> { 0 };
+
+var constant_1_2 = variables.First(v => v.Row == 1 && v.Column == 2);
+domains[constant_1_2] = new List<int> { 0 };
+
+var constant_1_3 = variables.First(v => v.Row == 1 && v.Column == 3);
+domains[constant_1_3] = new List<int> { 0 };
+
+var constant_1_5 = variables.First(v => v.Row == 1 && v.Column == 5);
+domains[constant_1_5] = new List<int> { 1 };
+
+var constant_2_1 = variables.First(v => v.Row == 2 && v.Column == 1);
+domains[constant_2_1] = new List<int> { 0 };
+
+var constant_2_2 = variables.First(v => v.Row == 2 && v.Column == 2);
+domains[constant_2_2] = new List<int> { 0 };
+
+var constant_2_5 = variables.First(v => v.Row == 2 && v.Column == 5);
+domains[constant_2_5] = new List<int> { 1 };
+
+var constant_4_0 = variables.First(v => v.Row == 4 && v.Column == 0);
+domains[constant_4_0] = new List<int> { 0 };
+
+var constant_4_1 = variables.First(v => v.Row == 4 && v.Column == 1);
+domains[constant_4_1] = new List<int> { 0 };
+
+var constant_4_3 = variables.First(v => v.Row == 4 && v.Column == 3);
+domains[constant_4_3] = new List<int> { 1 };
+
+var constant_5_1 = variables.First(v => v.Row == 5 && v.Column == 1);
+domains[constant_5_1] = new List<int> { 1 };
+
+var constant_5_4 = variables.First(v => v.Row == 5 && v.Column == 4);
+domains[constant_5_4] = new List<int> { 0 };
+
+var constant_5_5 = variables.First(v => v.Row == 5 && v.Column == 5);
+domains[constant_5_5] = new List<int> { 0 };
+
+var csp = new CSP<Field, int>(variables, domains);
+
+// equal row count constraint
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 0).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 1).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 2).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 3).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 4).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Row == 5).ToList()));
+
+// equal column count constraint
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 0).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 1).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 2).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 3).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 4).ToList()));
+csp.AddConstraint(new EqualCountConstraint(variables.Where(v => v.Column == 5).ToList()));
+
+var solution = csp.Backtracking(new Dictionary<Field, int>());
 if (solution is null)
 {
     Console.WriteLine("No solution");
