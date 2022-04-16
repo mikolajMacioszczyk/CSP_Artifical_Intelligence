d9737ebdb2d5e10ff1b01331a5c92e0276ef8207
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-04-16 10:17:51 +0200

    fill results

--- Lista2/Program.cs
+++ Lista2/Program.cs
@@ -6,16 +6,16 @@ using Lista2.Services;
 
 IValueHeuristic<int> GetValueHeuristic()
 {
-    return new NoHeuristic<int>();
+    return new RandomHeuristic<int>();
 }
 IVariableHeuristic<Field, int> GetVariableHeuristic()
 {
-    return new NoHeuristic<Field, int>();
+    return new SmallerDomainHeuristic<Field, int>();
 }
-int maxSolutions = int.MaxValue;
+int maxSolutions = 1;
 
 //var binaryInputs = new string[] { "binary_6x6", "binary_8x8", "binary_10x10" };
-var binaryInputs = new string[] { "binary_2x2", "binary_3x3", "binary_4x4", "binary_5x5", "binary_6x6", "binary_7x7", "binary_8x8", "binary_9x9", "binary_10x10", "binary_12x12" }; // binary_14x14
+var binaryInputs = new string[] { "binary_2x2", "binary_2x2", "binary_3x3", "binary_4x4", "binary_5x5", "binary_6x6", "binary_7x7", "binary_8x8", "binary_9x9", "binary_10x10", "binary_12x12" }; // binary_14x14
 //var binaryInputs = new string[] { };
 var futoshikiInputs = new string[] { "futoshiki_2x2", "futoshiki_3x3", "futoshiki_4x4", "futoshiki_5x5", "futoshiki_6x6", "futoshiki_7x7" }; 
 //var futoshikiInputs = new string[] { "futoshiki_8x8" };
